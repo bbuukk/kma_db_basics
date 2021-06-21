839e507828a5de4e3750c27b434bbcdd279b6129
Demon4ort <danylkodem@gmail.com>
2021-06-21 21:00:47 +0300

    fixed BookReader and BookInstance API

--- B-D-S12-PI/src/main/java/db/entities/BookInstance.java
+++ B-D-S12-PI/src/main/java/db/entities/BookInstance.java
@@ -2,51 +2,26 @@ package db.entities;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 
 public class BookInstance {
     private Integer id;
     private Integer shelf;
-    private LocalDate dateOut;
-    private LocalDate dateExp;
-    private LocalDate dateReturn;
     private Integer ISBN;
 
-    public BookInstance(Integer id, Integer shelf, LocalDate dateOut, LocalDate dateExp, LocalDate dateReturn, Integer ISBN) {
+    public BookInstance(Integer id, Integer shelf, Integer ISBN) {
         this.id = id;
         this.shelf = shelf;
-        this.dateOut = dateOut;
-        this.dateExp = dateExp;
-        this.dateReturn = dateReturn;
         this.ISBN = ISBN;
     }
 
-    public BookInstance(Integer shelf, LocalDate dateOut, LocalDate dateExp, LocalDate dateReturn, Integer ISBN) {
+    public BookInstance(Integer shelf, Integer ISBN) {
         this.shelf = shelf;
-        this.dateOut = dateOut;
-        this.dateExp = dateExp;
-        this.dateReturn = dateReturn;
         this.ISBN = ISBN;
     }
 
     public BookInstance(ResultSet resultSet) throws SQLException {
         this.id = resultSet.getInt("id_i");
         this.shelf = resultSet.getInt("shelf");
-        try {
-            this.dateOut = resultSet.getDate("date_out").toLocalDate();
-        } catch (NullPointerException e) {
-            this.dateOut = null;
-        }
-        try {
-            this.dateExp = resultSet.getDate("date_exp").toLocalDate();
-        } catch (NullPointerException e) {
-            this.dateExp = null;
-        }
-        try {
-            this.dateReturn = resultSet.getDate("date_return").toLocalDate();
-        } catch (NullPointerException e) {
-            this.dateReturn = null;
-        }
         this.ISBN = resultSet.getInt("ISBN");
     }
 
@@ -66,30 +41,6 @@ public class BookInstance {
         this.shelf = shelf;
     }
 
-    public LocalDate getDateOut() {
-        return dateOut;
-    }
-
-    public void setDateOut(LocalDate dateOut) {
-        this.dateOut = dateOut;
-    }
-
-    public LocalDate getDateExp() {
-        return dateExp;
-    }
-
-    public void setDateExp(LocalDate dateExp) {
-        this.dateExp = dateExp;
-    }
-
-    public LocalDate getDateReturn() {
-        return dateReturn;
-    }
-
-    public void setDateReturn(LocalDate dateReturn) {
-        this.dateReturn = dateReturn;
-    }
-
     public Integer getISBN() {
         return ISBN;
     }
@@ -103,9 +54,6 @@ public class BookInstance {
         return "BookInstance{" +
                 "id=" + id +
                 ", shelf=" + shelf +
-                ", dateOut=" + dateOut +
-                ", dateExp=" + dateExp +
-                ", dateReturn=" + dateReturn +
                 ", ISBN=" + ISBN +
                 '}';
     }
