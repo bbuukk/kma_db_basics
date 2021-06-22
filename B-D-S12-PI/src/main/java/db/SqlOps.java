a9644d7b92a18d49d3970f668f9e9267c84aaff7
Demon4ort <danylkodem@gmail.com>
2021-06-22 05:19:28 +0300

    complex queries

--- B-D-S12-PI/src/main/java/db/SqlOps.java
+++ B-D-S12-PI/src/main/java/db/SqlOps.java
@@ -1,12 +1,17 @@
 package db;
 
 import com.mysql.cj.jdbc.MysqlDataSource;
+import db.entities.Reader;
 import db.repos.*;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class SqlOps {
@@ -106,5 +111,174 @@ public class SqlOps {
     public ReaderRepository getReaderRepository() {
         return readerRepository;
     }
+
+
+    public List<Reader> getAllReadersNotReadAuthor(String authorName) {
+        if (authorName.isEmpty()) throw new IllegalArgumentException();
+        List<Reader> list = new ArrayList<>();
+        try (Statement st = connection.createStatement();
+             ResultSet resultSet = st.executeQuery(
+                     "SELECT * FROM mydb.Reader" +
+                             " WHERE id_r not in(" +
+                             "SELECT id_r FROM mydb.BookReader " +
+                             "where id_i in(" +
+                             "SELECT id_i from mydb.BookInstance " +
+                             "WHERE ISBN in(" +
+                             "SELECT ISBN from mydb.Authorship " +
+                             "WHERE id_a = (SELECT id_a FROM mydb.Author " +
+                             "WHERE name_a = '" + authorName + "'))))")) {
+
+            while (resultSet.next()) {
+                list.add(new Reader(resultSet));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Не вірний SQL запит на update");
+            e.printStackTrace();
+
+        }
+        return list;
+    }
+
+    public List<CityReaderNum> getNumReadersByCityWIthPrice(String requirement) {
+        if (requirement.isEmpty()) throw new IllegalArgumentException();
+        List<CityReaderNum> list = new ArrayList<>();
+        try (Statement st = connection.createStatement();
+             ResultSet resultSet = st.executeQuery(
+                     "Select city_r, Count(id_r) as reader_number " +
+                             "from mydb.Reader " +
+                             "where id_r in ( " +
+                             "select id_r " +
+                             "from mydb.BookReader " +
+                             "where id_i in ( " +
+                             "select id_i " +
+                             "from mydb.BookInstance " +
+                             "where ISBN in ( " +
+                             "select ISBN " +
+                             "from mydb.Book " +
+                             "where price " + requirement +
+                             "))) group by city_r")) {
+
+            while (resultSet.next()) {
+                list.add(new CityReaderNum(resultSet));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Не вірний SQL запит на update");
+            e.printStackTrace();
+
+        }
+        return list;
+    }
+
+    public List<AuthorBookNum> getNumBooksByAuthorForCategory(String nameCategory) {
+        if (nameCategory.isEmpty()) throw new IllegalArgumentException();
+        List<AuthorBookNum> list = new ArrayList<>();
+        try (Statement st = connection.createStatement();
+             ResultSet resultSet = st.executeQuery(
+                     "select mydb.Author.id_a, name_a, Count(ISBN) as book_number from mydb.Author inner join mydb.Authorship on Author.id_a = Authorship.id_a\n" +
+                             "where ISBN in (\n" +
+                             "\n" +
+                             "select ISBN\n" +
+                             "\n" +
+                             "from mydb.Belongs\n" +
+                             "\n" +
+                             "where id_c in (\n" +
+                             "\n" +
+                             "select id_c\n" +
+                             "\n" +
+                             "from mydb.Catalog\n" +
+                             "\n" +
+                             "where name_c = '" + nameCategory + "' " +
+                             "\n" +
+                             ")\n" +
+                             "\n" +
+                             ")\n" +
+                             "\n" +
+                             "group by Author.id_a")) {
+
+            while (resultSet.next()) {
+                list.add(new AuthorBookNum(resultSet));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Не вірний SQL запит на update");
+            e.printStackTrace();
+
+        }
+        return list;
+    }
+
+    //not working
+    public List<Reader> getAllReadersbyReaderAndCategory(String Pib, String nameCategory) {
+        if (nameCategory.isEmpty() || Pib.isEmpty()) throw new IllegalArgumentException();
+        List<Reader> list = new ArrayList<>();
+        try (Statement st = connection.createStatement();
+             ResultSet resultSet = st.executeQuery("select * from mydb.Reader " +
+                     "where not exists( " +
+                     "select ISBN " +
+                     "from mydb.BookInstance " +
+                     "where ISBN in( Select * from (select `mydb`.`BookInstance`.`ISBN` AS `ISBN`\n " +
+                     "from ((`mydb`.`BookInstance` join `mydb`.`BookReader` on ((`mydb`.`BookReader`.`id_i` = `mydb`.`BookInstance`.`id_i`)))\n" +
+                     "         join `mydb`.`Reader` on ((`mydb`.`BookReader`.`id_r` = `mydb`.`Reader`.`id_r`)))\n" +
+                     "where ((`mydb`.`Reader`.`PIB` = '" + Pib + "') and\n" +
+                     "       `mydb`.`BookInstance`.`ISBN` in (select `mydb`.`Belongs`.`ISBN`, `mydb`.`Belongs`.`id_c`\n" +
+                     "                                        from `mydb`.`Belongs`\n" +
+                     "                                        where (`mydb`.`Belongs`.`id_c` = (select `mydb`.`Catalog`.`id_c`\n" +
+                     "                                                                          from `mydb`.`Catalog`\n" +
+                     "                                                                          where (`mydb`.`Catalog`.`name_c` = '" + nameCategory + "')))));\n" + "))) " +
+                     "and ISBN not in( " +
+                     "Select ISBN From mydb.ALL_BOOKS " +
+                     "where mydb.Reader.id_r = ALL_BOOKS.id_r);")) {
+
+            while (resultSet.next()) {
+                list.add(new Reader(resultSet));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Не вірний SQL запит на update");
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    //requirement = "> 28";
+    //or " = 12 ";
+    static class CityReaderNum {
+        String city;
+        int num;
+
+        CityReaderNum(ResultSet resultSet) throws SQLException {
+            city = resultSet.getString("city_r");
+            num = resultSet.getInt("reader_number");
+        }
+
+        @Override
+        public String toString() {
+            return "CityReaderNum{" +
+                    "city='" + city + '\'' +
+                    ", num=" + num +
+                    '}';
+        }
+    }
+
+    static class AuthorBookNum {
+        String author;
+        int num;
+
+        AuthorBookNum(ResultSet resultSet) throws SQLException {
+            author = resultSet.getString("name_a");
+            num = resultSet.getInt("book_number");
+        }
+
+        @Override
+        public String toString() {
+            return "AuthorBookNum{" +
+                    "author='" + author + '\'' +
+                    ", num=" + num +
+                    '}';
+        }
+    }
+
 
 }
--- B-D-S12-PI/src/main/java/db/SqlTest.java
+++ B-D-S12-PI/src/main/java/db/SqlTest.java
@@ -3,7 +3,9 @@ package db;
 public class SqlTest {
     public static void main(String[] args) {
         SqlOps sqlOps = new SqlOps();
-        System.out.println(sqlOps.getAuthorRepository().getAllAuthors());
-
+        System.out.println(sqlOps.getAllReadersNotReadAuthor("shevchenko"));
+        System.out.println(sqlOps.getNumReadersByCityWIthPrice(" > 10"));
+        System.out.println(sqlOps.getNumBooksByAuthorForCategory("Novel"));
+        System.out.println(sqlOps.getAllReadersbyReaderAndCategory("Chervonenko Pavliv Yurich", "Science fiction"));
     }
 }
